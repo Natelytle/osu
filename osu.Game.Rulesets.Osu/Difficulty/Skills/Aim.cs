8f32533f164f0563d4db984859c89da14830cf96
Natelytle <nathenbr@gmail.com>
2023-10-16 18:32:02 -0400

    Change speed multiplier, implement FC time instead of aim prob

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -1,15 +1,18 @@
-﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+﻿
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
+
+#nullable disable
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using MathNet.Numerics;
+using MathNet.Numerics.RootFinding;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Difficulty.Skills;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Difficulty.Evaluators;
-using MathNet.Numerics.RootFinding;
+using static MathNet.Numerics.SpecialFunctions;
 
 namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 {
@@ -26,109 +29,44 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private readonly bool withSliders;
 
-        private double skillMultiplier => 128;
+        private double skillMultiplier => 125;
 
-        private const double fc_probability = 0.02;
-        private const int bin_count = 32;
+        // Assume players spend 20 minutes retrying a map before they FC
+        private const double time_threshold = 20;
 
         private double currentStrain;
 
         private double strainDecayBase => 0.15;
 
         private readonly List<double> difficulties = new List<double>();
+        private readonly List<double> deltaTimes = new List<double>();
 
-        private static double hitProbability(double skill, double difficulty) => SpecialFunctions.Erf(skill / (Math.Sqrt(2) * difficulty));
-
-        private struct Bin
-        {
-            public double Difficulty;
-            public double Count;
-
-            public double FcProbability(double skill)
-            {
-                return Math.Pow(hitProbability(skill, Difficulty), Count);
-            }
-        }
-
-        private static double fcProbability(double skill, IEnumerable<Bin> bins)
-        {
-            if (skill <= 0) return 0;
-
-            return bins.Aggregate(1.0, (current, bin) => current * bin.FcProbability(skill));
-        }
-
-        /// <summary>
-        /// Create an array of equally spaced bins. Count is linearly interpolated into each bin.
-        /// For example, if we have bins with values [1,2,3,4,5] and want to insert the value 3.2,
-        /// we will add 0.8 to the count of 3's and 0.2 to the count of 4's
-        /// </summary>
-        private Bin[] createBins(double maxDifficulty)
-        {
-            var bins = new Bin[bin_count];
-
-            for (int i = 0; i < bin_count; i++)
-            {
-                bins[i].Difficulty = maxDifficulty * (i + 1) / bin_count;
-            }
-
-            foreach (double d in difficulties)
-            {
-                double binIndex = bin_count * (d / maxDifficulty) - 1;
-
-                int lowerBound = (int)Math.Floor(binIndex);
-                double t = binIndex - lowerBound;
-
-                //This can be -1, corresponding to the zero difficulty bucket.
-                //We don't store that since it doesn't contribute to difficulty
-                if (lowerBound >= 0)
-                {
-                    bins[lowerBound].Count += (1 - t);
-                }
-
-                int upperBound = lowerBound + 1;
-
-                // this can be == bin_count for the maximum difficulty object, in which case t will be 0 anyway
-                if (upperBound < bin_count)
-                {
-                    bins[upperBound].Count += t;
-                }
-            }
-
-            return bins;
-        }
-
-        private double difficultyValueBinned()
-        {
-            double maxDiff = difficulties.Max();
-            if (maxDiff <= 1e-10) return 0;
-
-            var bins = createBins(maxDiff);
-
-            double lowerBoundEstimate = 0.5 * maxDiff;
-            double upperBoundEstimate = 3.0 * maxDiff;
-
-            double skill = Brent.FindRootExpand(
-                skill => fcProbability(skill, bins) - fc_probability,
-                lowerBoundEstimate,
-                upperBoundEstimate,
-                1e-4);
-
-            return skill;
-        }
+        private static double hitProbability(double skill, double difficulty) => Erf(skill / (Math.Sqrt(2) * difficulty));
 
         public override void Process(DifficultyHitObject current)
         {
             difficulties.Add(strainValueAt(current));
+            // Cap deltatimes at 5 seconds to not reward breaks
+            deltaTimes.Add(Math.Min(current.DeltaTime, 5000));
         }
 
-        private double fcProbability(double skill)
+        private double fcTime(double skill)
         {
-            if (skill <= 0) return 0;
+            if (skill <= 0) return double.PositiveInfinity;
 
-            return difficulties.Aggregate<double, double>(1, (current, d) => current * hitProbability(skill, d));
+            double t = 0;
+            double prodOfHitProbabilities = 1;
+
+            for (int n = difficulties.Count - 1; n >= 0; n--)
+            {
+                prodOfHitProbabilities *= hitProbability(skill, difficulties[n]);
+                t += deltaTimes[n] / prodOfHitProbabilities - deltaTimes[n];
+            }
+
+            return t;
         }
 
-        private double difficultyValueExact()
+        public override double DifficultyValue()
         {
             double maxDiff = difficulties.Max();
             if (maxDiff <= 1e-10) return 0;
@@ -137,20 +75,12 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
             double upperBoundEstimate = 3.0 * maxDiff;
 
             double skill = Brent.FindRootExpand(
-                skill => fcProbability(skill) - fc_probability,
+                skill => fcTime(skill) - time_threshold * 60000,
                 lowerBoundEstimate,
                 upperBoundEstimate,
                 1e-4);
 
             return skill;
-        }
-
-        public override double DifficultyValue()
-        {
-            if (difficulties.Count == 0)
-                return 0;
-
-            return difficulties.Count < 2 * bin_count ? difficultyValueExact() : difficultyValueBinned();
         }
 
         private double strainDecay(double ms) => Math.Pow(strainDecayBase, ms / 1000);
