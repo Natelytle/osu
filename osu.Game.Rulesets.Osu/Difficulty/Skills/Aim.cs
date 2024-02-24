7ef762724df6ad66885dc9d8599fb67f771965af
nathen <nathenbr@gmail.com>
2024-02-24 14:07:33 -0500

    Implement difficulty-based aim strain

--- osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
+++ osu.Game.Rulesets.Osu/Difficulty/Skills/Aim.cs
@@ -2,6 +2,7 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
+using System.Collections.Generic;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Mods;
 using osu.Game.Rulesets.Osu.Difficulty.Evaluators;
@@ -21,21 +22,53 @@ namespace osu.Game.Rulesets.Osu.Difficulty.Skills
 
         private readonly bool withSliders;
 
-        private double currentStrain;
+        private readonly List<double> previousStrains = new List<double>();
 
         private double skillMultiplier => 23.55;
         private double strainDecayBase => 0.15;
 
-        private double strainDecay(double ms) => Math.Pow(strainDecayBase, ms / 1000);
+        protected override double CalculateInitialStrain(double time, DifficultyHitObject current)
+        {
+            double priorDifficulty = highestPreviousStrain(previousStrains, current, time - current.Previous(0).StartTime);
 
-        protected override double CalculateInitialStrain(double time, DifficultyHitObject current) => currentStrain * strainDecay(time - current.Previous(0).StartTime);
+            return getStrainWithPrior(0, priorDifficulty, 3);
+        }
 
         protected override double StrainValueAt(DifficultyHitObject current)
         {
-            currentStrain *= strainDecay(current.DeltaTime);
-            currentStrain += AimEvaluator.EvaluateDifficultyOf(current, withSliders) * skillMultiplier;
+            double currentDifficulty = AimEvaluator.EvaluateDifficultyOf(current, withSliders) * skillMultiplier;
+            double priorDifficulty = highestPreviousStrain(previousStrains, current, current.DeltaTime);
+
+            double currentStrain = getStrainWithPrior(currentDifficulty, priorDifficulty, 3);
+            previousStrains.Add(currentStrain);
 
             return currentStrain;
+        }
+
+        private double getStrainWithPrior(double currentDifficulty, double priorDifficulty, double priorDiffInfluence)
+            => (priorDifficulty * priorDiffInfluence + currentDifficulty) / (priorDiffInfluence + 1);
+
+        private double highestPreviousStrain(List<double> previousStrains, DifficultyHitObject current, double time)
+        {
+            double hardestPreviousDifficulty = 0;
+            double cumulativeDeltatime = time;
+
+            double timeDecay(double ms) => Math.Pow(strainDecayBase, Math.Pow(ms / 900, 7));
+
+            for (int i = 0; i < previousStrains.Count; i++)
+            {
+                if (cumulativeDeltatime > 1200)
+                {
+                    previousStrains.RemoveRange(0, i);
+                    break;
+                }
+
+                hardestPreviousDifficulty = Math.Max(hardestPreviousDifficulty, previousStrains[^(i + 1)] * timeDecay(cumulativeDeltatime));
+
+                cumulativeDeltatime += current.Previous(i).DeltaTime;
+            }
+
+            return hardestPreviousDifficulty;
         }
     }
 }
