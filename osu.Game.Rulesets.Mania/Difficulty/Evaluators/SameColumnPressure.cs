159d02f91867bcc6d00a513370785dd6ace996e7
Nathen <nathenbr@gmail.com>
2025-03-24 10:07:25 -0400

    Reduce array allocations in SameColumnPressure.cs

--- osu.Game.Rulesets.Mania/Difficulty/Evaluators/SameColumnPressure.cs
+++ osu.Game.Rulesets.Mania/Difficulty/Evaluators/SameColumnPressure.cs
@@ -13,15 +13,18 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Evaluators
     {
         public static double[] EvaluateSameColumnPressure(List<ManiaDifficultyHitObject>[] perColumnNoteList, int totalColumns, int mapLength, double hitLeniency)
         {
-            double[][] perColumnPressure = new double[totalColumns][];
-            double[][] perColumnDeltaTimes = new double[totalColumns][];
+            double[] sameColumnPressure = new double[mapLength];
+            double[] perColumnPressure = new double[mapLength];
+            double[] perColumnDeltaTimes = new double[mapLength];
+            double[] sumWeights = new double[mapLength];
+            double[] sumValLambdaWeights = new double[mapLength];
 
             for (int col = 0; col < totalColumns; col++)
             {
                 IEnumerable<ManiaDifficultyHitObject> columnNotes = perColumnNoteList[col];
 
-                perColumnPressure[col] = new double[mapLength];
-                perColumnDeltaTimes[col] = new double[mapLength];
+                Array.Clear(perColumnPressure);
+                Array.Clear(perColumnDeltaTimes);
 
                 ManiaDifficultyHitObject? prev = null;
 
@@ -35,41 +38,32 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Evaluators
                         // the variables created earlier are filled with delta/val
                         for (int t = (int)prev.StartTime; t < note.StartTime; t++)
                         {
-                            perColumnDeltaTimes[col][t] = delta;
-                            perColumnPressure[col][t] = val;
+                            perColumnPressure[t] = val;
+                            perColumnDeltaTimes[t] = delta;
                         }
                     }
 
                     prev = note;
                 }
 
-                perColumnPressure[col] = ListUtils.ApplySymmetricMovingAverage(perColumnPressure[col], 500);
-            }
+                perColumnPressure = ListUtils.ApplySymmetricMovingAverage(perColumnPressure, 500);
 
-            double[] sameColumnPressure = new double[mapLength];
+                // Process and accumulate weighted values directly
+                for (int t = 0; t < mapLength; t++)
+                {
+                    double val = perColumnPressure[t];
+                    double weight = perColumnDeltaTimes[t] > 0 ? 1.0 / perColumnDeltaTimes[t] : 0;
+
+                    sumWeights[t] += weight;
+                    sumValLambdaWeights[t] += Math.Pow(val, SunnySkill.LAMBDA_N) * weight;
+                }
+            }
 
             for (int t = 0; t < mapLength; t++)
             {
-                double sumWeights = 0;
-                double sumValLambdaWeight = 0;
+                double firstPart = sumWeights[t] > 0 ? sumValLambdaWeights[t] / sumWeights[t] : 0;
 
-                for (int col = 0; col < totalColumns; col++)
-                {
-                    double val = perColumnPressure[col][t];
-                    double weight = perColumnDeltaTimes[col][t] > 0 ? 1.0 / perColumnDeltaTimes[col][t] : 0;
-
-                    sumWeights += weight;
-                    sumValLambdaWeight += Math.Pow(val, SunnySkill.LAMBDA_N) * weight;
-                }
-
-                double firstPart = sumWeights > 0 ? sumValLambdaWeight / sumWeights : 0;
-
-                double weightedAverage = Math.Pow(
-                    firstPart,
-                    1.0 / SunnySkill.LAMBDA_N
-                );
-
-                sameColumnPressure[t] = weightedAverage;
+                sameColumnPressure[t] = Math.Pow(firstPart, 1.0 / SunnySkill.LAMBDA_N);
             }
 
             return sameColumnPressure;
