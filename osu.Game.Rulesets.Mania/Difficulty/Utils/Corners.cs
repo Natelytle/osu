cd7c468bbb04ba8be975843adbe8aab03356323a
Nathen <nathenbr@gmail.com>
2025-06-27 19:50:06 -0400

    Optimize corners

--- osu.Game.Rulesets.Mania/Difficulty/Utils/Corners.cs
+++ osu.Game.Rulesets.Mania/Difficulty/Utils/Corners.cs
@@ -1,7 +1,6 @@
 // Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using System;
 using System.Collections.Generic;
 using osu.Game.Extensions;
 using osu.Game.Rulesets.Mania.Difficulty.Preprocessing;
@@ -42,26 +41,21 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Utils
 
         public readonly void AddCornersForNote(ManiaDifficultyHitObject note)
         {
-            // Do not add corner locations before the start of the map.
-            double baseCornerStartTime = Math.Max(note.StartTime - 499, 0);
-            double aCornerStartTime = Math.Max(note.StartTime - 1000, 0);
+            BaseCorners.AddRange([note.StartTime, note.EndTime]);
+            ACorners.AddRange([note.StartTime, note.EndTime]);
+            AllCorners.AddRange([note.StartTime, note.EndTime]);
 
-            // Same but for the end of the map.
-            double[] baseCornerEndTimes = new[] { Math.Min(note.StartTime + 1, mapEndTime), Math.Min(note.StartTime + 500, mapEndTime) };
-            double aCornerEndTime = Math.Min(note.StartTime + 1000, mapEndTime);
+            if (note.StartTime <= mapEndTime - 1)
+            {
+                BaseCorners.Add(note.StartTime + 1);
+                AllCorners.Add(note.StartTime + 1);
+            }
 
-            BaseCorners.Add(note.StartTime);
-            ACorners.Add(note.StartTime);
-            AllCorners.Add(note.StartTime);
-
-            BaseCorners.Add(baseCornerStartTime);
-            ACorners.Add(aCornerStartTime);
-            AllCorners.AddRange([baseCornerStartTime, aCornerStartTime]);
-
-            BaseCorners.AddRange(baseCornerEndTimes);
-            ACorners.Add(aCornerEndTime);
-            AllCorners.AddRange(baseCornerEndTimes);
-            AllCorners.Add(aCornerEndTime);
+            if (note.EndTime <= mapEndTime - 1)
+            {
+                BaseCorners.Add(note.EndTime + 1);
+                AllCorners.Add(note.EndTime + 1);
+            }
         }
     }
 }
