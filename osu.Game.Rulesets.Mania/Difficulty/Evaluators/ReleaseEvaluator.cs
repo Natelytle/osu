23f91981d38a8251737addc080b8f0898f8bd296
Natelytle <nathenbr@gmail.com>
2026-01-19 18:13:49 -0500

    add release junk

--- osu.Game.Rulesets.Mania/Difficulty/Evaluators/ReleaseEvaluator.cs
+++ osu.Game.Rulesets.Mania/Difficulty/Evaluators/ReleaseEvaluator.cs
@@ -10,6 +10,8 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Evaluators
 {
     public static class ReleaseEvaluator
     {
+        private const double difficulty_multiplier = 1.0;
+
         public static double EvaluateDifficultyOf(ManiaDifficultyHitObject current)
         {
             ManiaDifficultyHitObject? nextTail = current.NextTail(0);
@@ -29,7 +31,11 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Evaluators
             // Multiplier for the individual difficulties of these notes.
             releaseDifficulty *= 1.0 + 0.8 * (currentDifficulty + nextDifficulty);
 
-            return releaseDifficulty;
+            // Divide release difficulty by the number of notes within a +-500ms window (probably to nerf high pressing difficulty situations)
+            double localNoteCount = getHeadCountWithinTimeWindow(500, current);
+            releaseDifficulty *= 35.0 / (localNoteCount + 8.0);
+
+            return releaseDifficulty * difficulty_multiplier;
         }
 
         private static double evaluateIndividualDifficultyOf(ManiaDifficultyHitObject current)
@@ -60,6 +66,31 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Evaluators
             double lt = DifficultyCalculationUtils.Logistic(timingDifficultyComponent, 0.75, 5.0);
 
             return 2.0 * (lh * lt) / (lh + lt);
+        }
+
+        private static double getHeadCountWithinTimeWindow(double window, ManiaDifficultyHitObject current)
+        {
+            int count = 1;
+
+            var previous = current.PrevHead(0);
+
+            while (previous is not null && previous.StartTime > current.ActualTime - window)
+            {
+                count++;
+
+                previous = previous.PrevHead(0);
+            }
+
+            var next = current.NextHead(0);
+
+            while (next is not null && next.StartTime < current.ActualTime + window)
+            {
+                count++;
+
+                next = next.NextHead(0);
+            }
+
+            return count;
         }
     }
 }
